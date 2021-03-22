24d966ef460ef6f5435f3609ea68e39b587e3b64
WillamsPinto <willams.pinto@dcx.ufpb.br>
2021-03-22 00:24:55 -0300

    Destaque ao selecionar uma op. do Sumário

--- js/script.js
+++ js/script.js
@@ -16,4 +16,14 @@ function videoController(){
 
 }
 
+function mostrarAtivo(tag){
+    var tag_li = document.getElementById('item_menu');
+    var tag_a = tag_li.getElementsByTagName('a');
+    for (i=0; i<tag_a.length; i++ )
+    {
+       tag_a[i].style.color = "";
+    }
+       tag.style.color = "#ff0000";
+    }
+
 
