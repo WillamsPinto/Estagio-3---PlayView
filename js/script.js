adf8a36ba93cbf3d39e1939314b93687d65ac032
willamspinto <willams.pinto@fastsolucoes.com.br>
2021-03-29 23:26:09 -0300

    Função de parar video

--- js/script.js
+++ js/script.js
@@ -45,10 +45,35 @@ function closeDiv() {
         var el =document.getElementById(div[cont]);
         el.style.width = "0";
     }
+    stopVideo();
 }
 
 function desativo(id){
     var tag_a = document.getElementById(id);
     tag_a.style.color = "";
 }
+
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// global variable for the player
+var player;
+
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+    // create the global player from the specific iframe (#video)
+    player = new YT.Player('video');
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+
+function pauseVideo() {
+    player.pauseVideo();
+}
+
 
