a17d27f63cec024c1cc6481d8917a8412b0b7fad
willamspinto <willams.pinto@fastsolucoes.com.br>
2021-03-31 00:12:21 -0300

    Nova tentativa de adicionar o sendgrid

--- email.php
+++ email.php
@@ -1,28 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+		$nome = $_POST['nome'];
+		$email = $_POST['email'];
+		$mensagem = $_POST['mensagem'];
+		
+        require 'vendor/autoload.php';
 
-if(isset($_POST['email']) && (!empty($_POST['email']))){
+        $from = new SendGrid\Email(null, "willamspinto18@gmail.com");
+        $subject = "Mensagem de contato";
+        $to = new SendGrid\Email(null, "arianny995@uorak.com");
+        $content = new SendGrid\Content("text/html", "Olá Cesar, <br><br>Nova mensagem de contato<br><br>Nome: $nome<br>Email: $email <br>Mensagem: $mensagem");
+        $mail = new SendGrid\Mail($from, $subject, $to, $content);
+        
+        $key      = parse_ini_file('sendgrid.ini')['key'];
+        $sendgrid = new \SendGrid($key);
 
-$nome = addslashes($_POST['nome']);
-$email = addslashes($_POST['email']);
-$mensagem = addslashes($_POST['mensagem']);
-
-$to = "willamspinto18@gmail.com";
-$subject = "Contato - PlayView";
-$body = "Nome: ".$nome. "\r\n".
-        "E-mail: ".$email. "\r\n".
-        "Mensagem: ".$mensagem;
-
-$header = "From: leia7654@uorak.com"."\r\n"."Reply-to:".$email."\r\n"."X=Mailer:PHP/".phpversion();
-
-
-if(mail($to, $subject, $body, $header)){
-    echo("E-mail enviado com sucesso");
-
-}else{
-    echo("E-mail não pode ser enviado");
-}
-
-
-}
-
-?>
+        $response = $sendgrid->client->mail()->send()->post($mail);
+        echo "Mensagem enviada com sucesso";
+		
+        ?>
+    </body>
+</html>
